f9f73c25546c34eacada989c59994dbd5f5118e1
Enrique Bernabeu <ebvcontacto@gmail.com>
2023-10-11 23:38:23 +0200

    Add HTTPS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import { createServer } from './server'
 import getEnvVariable from './lib/env'
+import https from 'https'
+import { certificateFor } from 'devcert'
 
 const PORT = getEnvVariable('PORT')
+const useSsl = process.argv.includes('--ssl')
 
-const app = createServer()
+async function main() {
+  const app = createServer()
 
-app.listen(PORT, () => console.log('Server started at: http://localhost:%d', PORT))
+  if (!useSsl) {
+    app.listen(PORT, () => console.log('Server running on http://localhost:%d', PORT))
+    return
+  }
+
+  const ssl = await certificateFor('localhost')
+
+  https
+    .createServer(
+      {
+        key: ssl.key,
+        cert: ssl.cert,
+      },
+      createServer()
+    )
+    .listen(3000, () => {
+      console.log('Server running on https://localhost:%d', PORT)
+    })
+}
+
+main()
